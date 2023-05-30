3d4f58a6572aa2c67a34b69aba143329bc70dade
Thiago Willecke <thiagogwillecke@gmail.com>
2023-05-30 14:34:55 -0300

    Add api endpoint to retrieve residential properties by job id

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,11 @@ app.get("/api/residential-properties", async function (req, res){
     const residential_properties = await residentialPropertiesService.getResidentialProperties();
     res.json(residential_properties)
 })
+app.get("/api/residential-properties/job-id/:job_id", async function (req, res) {
+    const job_id = req.params.job_id;
+    const residentialPropertiesService = new ResidentialPropertiesService();
+    const residentialProperties = await residentialPropertiesService.getResidentialPropertiesByJobId(job_id);
+    res.json(residentialProperties);
+});
 app.listen(config.server.port);
 
--- src/service/ResidentialPropertyService.ts
+++ src/service/ResidentialPropertyService.ts
@@ -11,7 +11,38 @@ export default class ResidentialPropertiesService {
         const residential_properties_data = await connection.query(
             `select 
                 address, region, city, state, housing_type, rent_type, price, datetime, cond_price, iptu_price, size_m2, bedroom_count, parking_count, bathroom_count 
-                from public.rent_data LIMIT 10`,
+                from public.rent_data`,
+                []);
+        const residential_properties: ResidentialProperty[] = [];
+        for (const property_data of residential_properties_data) {
+            residential_properties.push(
+                new ResidentialProperty(
+                    property_data.address,
+                    property_data.region,
+                    property_data.city,
+                    property_data.state,
+                    property_data.housing_type,
+                    property_data.rent_type,
+                    property_data.price,
+                    property_data.datetime,
+                    property_data.cond_price,
+                    property_data.iptu_price,
+                    property_data.size_m2,
+                    property_data.bedroom_count,
+                    property_data.parking_count,
+                    property_data.bathroom_count
+                    ))
+        }
+        await connection.$pool.end();
+        return residential_properties;
+    }
+
+    async getResidentialPropertiesByJobId (job_id: string) {
+        const connection = pgp()(config.database)
+        const residential_properties_data = await connection.query(
+            `select 
+                job_id, address, region, city, state, housing_type, rent_type, price, datetime, cond_price, iptu_price, size_m2, bedroom_count, parking_count, bathroom_count 
+                from public.rent_data where job_id = ${job_id}`,
                 []);
         const residential_properties: ResidentialProperty[] = [];
         for (const property_data of residential_properties_data) {
