2009e181e539f477f0dc9beefb7280b6ce3254f9
Thiago Willecke <thiagogwillecke@gmail.com>
2023-06-02 14:45:21 -0300

    Add findAllByJobId() to ResidentialPropertyRepositoryDatabase

--- src/domain/repository/ResidentialPropertyRepository.ts
+++ src/domain/repository/ResidentialPropertyRepository.ts
@@ -1,5 +1,6 @@
 import ResidentialProperty from "../entity/ResidentialProperty";
 
 export default interface ResidentialPropertyRepository {
-    findAll (): Promise<ResidentialProperty[]>
+    findAll (): Promise<ResidentialProperty[]>,
+    findAllByJodId (job_id: number): Promise<ResidentialProperty[]>
 }
--- src/service/ResidentialPropertyService.ts
+++ src/service/ResidentialPropertyService.ts
@@ -13,33 +13,7 @@ export default class ResidentialPropertiesService {
     }
 
     async getResidentialPropertiesByJobId (job_id: number) {
-        const connection = pgp()(config.database)
-        const residential_properties_data = await connection.query(
-            `select 
-                job_id, address, region, city, state, housing_type, rent_type, price, datetime, cond_price, iptu_price, size_m2, bedroom_count, parking_count, bathroom_count 
-                from public.rent_data where job_id = $1`,
-                [job_id]);
-        const residential_properties: ResidentialProperty[] = [];
-        for (const property_data of residential_properties_data) {
-            residential_properties.push(
-                new ResidentialProperty(
-                    property_data.address,
-                    property_data.region,
-                    property_data.city,
-                    property_data.state,
-                    property_data.housing_type,
-                    property_data.rent_type,
-                    property_data.price,
-                    property_data.datetime,
-                    property_data.cond_price,
-                    property_data.iptu_price,
-                    property_data.size_m2,
-                    property_data.bedroom_count,
-                    property_data.parking_count,
-                    property_data.bathroom_count
-                    ))
-        }
-        await connection.$pool.end();
+        const residential_properties = await this.residentialPropertyRepository.findAllByJodId(job_id);
         return residential_properties;
     }
 }
