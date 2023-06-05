55aad0de149549c22726509109af0431dfb7c593
Thiago Willecke <thiagogwillecke@gmail.com>
2023-06-05 14:08:52 -0300

    Code refactoring: add RouteController; add ExpressAdapter; Add Http interface

---
+++ src/infra/controller/RouteController.ts
@@ -0,0 +1,40 @@
+import ResidentialPropertiesService from "../../service/ResidentialPropertyService";
+import Http from "../http/Http";
+import ResidentialPropertyRepositoryDatabase from "../repository/ResidentialPropertyRepositoryDatabase";
+import Connection from "../database/Connection";
+import DataOverviewRepositoryDatabase from "../repository/DataOverviewRepositoryDatabase";
+import DataOverviewService from "../../service/DataOverviewService";
+
+export default class RouteController {
+
+    constructor(readonly http: Http, readonly connection: Connection) {
+        http.route("get", "/api/residential-properties", async function (params: any, body: any){
+            const residentialPropertyRepository = new ResidentialPropertyRepositoryDatabase(connection);
+            const residentialPropertiesService = new ResidentialPropertiesService(residentialPropertyRepository);
+            const residential_properties = await residentialPropertiesService.getResidentialProperties();
+            return residential_properties;
+        });
+
+        http.route("get", "/api/residential-properties/job-id/:job_id", async function (params: any, body: any){
+            const residentialPropertyRepository = new ResidentialPropertyRepositoryDatabase(connection);
+            const residentialPropertiesService = new ResidentialPropertiesService(residentialPropertyRepository);
+            const residentialProperties = await residentialPropertiesService.getResidentialPropertiesByJobId(parseInt(params.job_id));
+            return residentialProperties;
+        });
+
+        http.route("get", "/api/residential-properties/overview", async function (params: any, body: any){
+            const dataOverviewRepository = new DataOverviewRepositoryDatabase(connection);
+            const dataOverviewService = new DataOverviewService(dataOverviewRepository);
+            const data_overview = await dataOverviewService.getDataOverview();
+            return data_overview;
+        });
+
+        http.route("get", "/api/residential-properties/overview/job-id/:job_id", async function (params: any, body: any){
+            const dataOverviewRepository = new DataOverviewRepositoryDatabase(connection);
+            const dataOverviewService = new DataOverviewService(dataOverviewRepository);
+            const data_overview = await dataOverviewService.getDataOverviewByJobId(parseInt(params.job_id));            
+            return data_overview;
+        });
+    }
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,12 @@
-import express from "express";
 import config from "./config";
-import ResidentialPropertiesService from "./service/ResidentialPropertyService";
-import DataOverviewService from "./service/DataOverviewService";
 import PgPromiseConnection from "./infra/database/PgPromiseConnection";
-import ResidentialPropertyRepositoryDatabase from "./infra/repository/ResidentialPropertyRepositoryDatabase";
-import DataOverviewRepositoryDatabase from "./infra/repository/DataOverviewRepositoryDatabase";
-import cors from "cors"
-const app = express();
-app.use(cors());
+import ExpressAdapter from "./infra/http/ExpressAdapter";
+import RouteController from "./infra/controller/RouteController";
+
 
 const connection = new PgPromiseConnection;
-const residentialPropertyRepository = new ResidentialPropertyRepositoryDatabase(connection)
-const dataOverviewRepository = new DataOverviewRepositoryDatabase(connection);
+const http = new ExpressAdapter;
+new RouteController(http, connection);
+http.listen(config.server.port);
 
-app.get("/api/residential-properties", async function (req, res){
-    const residentialPropertiesService = new ResidentialPropertiesService(residentialPropertyRepository);
-    const residential_properties = await residentialPropertiesService.getResidentialProperties();
-    res.json(residential_properties)
-})
-app.get("/api/residential-properties/job-id/:job_id", async function (req, res) {
-    const job_id = req.params.job_id;
-    const residentialPropertiesService = new ResidentialPropertiesService(residentialPropertyRepository);
-    const residentialProperties = await residentialPropertiesService.getResidentialPropertiesByJobId(parseInt(job_id));
-    res.json(residentialProperties);
-});
-app.get("/api/residential-properties/overview", async function (req, res){
-    const dataOverviewService = new DataOverviewService(dataOverviewRepository);
-    const data_overview = await dataOverviewService.getDataOverview();
-    res.json(data_overview)
-})
-app.get("/api/residential-properties/overview/job-id/:job_id", async function (req, res){
-    const job_id = req.params.job_id;
-    const dataOverviewService = new DataOverviewService(dataOverviewRepository);
-    const data_overview = await dataOverviewService.getDataOverviewByJobId(parseInt(job_id));
-    res.json(data_overview)
-})
-app.listen(config.server.port);
 
