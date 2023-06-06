6c7d5c80d9e26d2510bf353bc7859ffe5a638aed
Thiago Willecke <thiagogwillecke@gmail.com>
2023-06-06 14:36:13 -0300

    Update DataOverviewRepositoryDatabase query

--- src/domain/entity/DataOverview.ts
+++ src/domain/entity/DataOverview.ts
@@ -1,31 +1,62 @@
-export default class DataOverview{
-    constructor (
+export default class DataOverview {
+    constructor(
         readonly job_id: number | string,
-        readonly date: number | string,
-        readonly average_price: number | string,
-        readonly average_price_by_rent_type_monthly: number | string,
-        readonly average_price_by_rent_type_daily: number | string,
-        readonly average_price_by_apartamento: number | string,
-        readonly average_price_by_sobrado: number | string,
-        readonly average_price_by_casa: number | string,
-        readonly average_price_by_studio: number | string,
-        readonly average_price_by_quitinete: number | string,
-        readonly average_size_in_m2: number | string,
-        readonly average_bedroom_count: number | string,
-        readonly average_parking_count?: number | string,
-        ){
-            if (average_parking_count === null) throw new Error("Region is required");
-            if (average_bedroom_count === null) throw new Error("City is required");
-            if (average_size_in_m2 === null) throw new Error("State is required");
-            if (average_price_by_apartamento === null) throw new Error("Housing type is required");
-            if (average_price_by_sobrado === null) throw new Error("Housing type is required");
-            if (average_price_by_casa === null) throw new Error("Housing type is required");
-            if (average_price_by_studio === null) throw new Error("Housing type is required");
-            if (average_price_by_quitinete === null) throw new Error("Housing type is required");
-            if (average_price_by_rent_type_monthly === null) throw new Error("Rent type is required");
-            if (average_price === null) throw new Error("Average price is required");
-            if (job_id === null) throw new Error("Job ID is required");
-            if (date === null) throw new Error("Date is required");
-
+        readonly total_count: number | string,
+        readonly total_avg: number | string,
+        readonly total_avg_monthly_count: number | string,
+        readonly total_avg_monthly: number | string,
+        readonly total_avg_daily_count: number | string,
+        readonly total_avg_daily: number | string,
+        readonly apartamento_monthly_count: number | string,
+        readonly apartamento_monthly_avg: number | string,
+        readonly apartamento_daily_count: number | string,
+        readonly apartamento_daily_avg: number | string,
+        readonly casa_monthly_count: number | string,
+        readonly casa_monthly_avg: number | string,
+        readonly casa_daily_count: number | string,
+        readonly casa_daily_avg: number | string,
+        readonly chacara_monthly_count: number | string,
+        readonly chacara_monthly_avg: number | string,
+        readonly chacara_daily_count: number | string,
+        readonly chacara_daily_avg: number | string,
+        readonly cobertura_monthly_count: number | string,
+        readonly cobertura_monthly_avg: number | string,
+        readonly cobertura_daily_count: number | string,
+        readonly cobertura_daily_avg: number | string,
+        readonly condominio_monthl_count: number | string,
+        readonly condominio_monthly_avg: number | string,
+        readonly condominio_daily_count: number | string,
+        readonly condominio_daily_avg: number | string,
+        readonly flat_monthly_count: number | string,
+        readonly flat_daily_count: number | string,
+        readonly flat_daily_avg: number | string,
+        readonly loft_monthly_count: number | string,
+        readonly loft_monthly_avg: number | string,
+        readonly loft_daily_count: number | string,
+        readonly loft_daily_avg: number | string,
+        readonly quitinete_monthly_count: number | string,
+        readonly quitinete_monthly_avg: number | string,
+        readonly quitinete_daily_count: number | string,
+        readonly quitinete_daily_avg: number | string,
+        readonly sobrado_monthly_count: number | string,
+        readonly sobrado_monthly_avg: number | string,
+        readonly sobrado_daily_count: number | string,
+        readonly sobrado_daily_avg: number | string,
+        readonly studio_monthly_count: number | string,
+        readonly studio_monthly_avg: number | string,
+        readonly studio_daily_count: number | string,
+        readonly studio_daily_avg: number | string,
+        readonly vila_monthly_count: number | string,
+        readonly vila_monthly_avg: number | string,
+        readonly vila_daily_count: number | string,
+        readonly vila_daily_avg: number | string,
+    ) {
+        if (job_id === null) throw new Error("Job ID is required");
+        if (total_count === null) throw new Error("Total count is required");
+        if (total_avg === null) throw new Error("Total average is required");
+        if (total_avg_monthly_count === null) throw new Error("Total average monthly count is required");
+        if (total_avg_monthly === null) throw new Error("Total average monthly is required");
+        if (total_avg_daily_count === null) throw new Error("Total average daily count is required");
+        if (total_avg_daily === null) throw new Error("Total average daily is required");
     }
 }
--- src/infra/repository/DataOverviewRepositoryDatabase.ts
+++ src/infra/repository/DataOverviewRepositoryDatabase.ts
@@ -10,92 +10,239 @@ export default class DataOverviewRepositoryDatabase implements DataOverviewRepos
     async findAll(): Promise<DataOverview[]> {
         const residential_properties_data = await this.connection.query(`
         SELECT
-        job_id,
-        DATE(datetime) AS date,
-        ROUND(AVG(price), 2) AS average_price,
-        ROUND(AVG(price) FILTER (WHERE rent_type = 'monthly'), 2) AS average_price_by_rent_type_monthly,
-        ROUND(AVG(price) FILTER (WHERE rent_type = 'daily'), 2) AS average_price_by_rent_type_daily,
-        ROUND(AVG(price) FILTER (WHERE housing_type = 'apartamento'), 2) AS average_price_by_apartamento,
-        ROUND(AVG(price) FILTER (WHERE housing_type = 'sobrado'), 2) AS average_price_by_sobrado,
-        ROUND(AVG(price) FILTER (WHERE housing_type = 'casa'), 2) AS average_price_by_casa,
-        ROUND(AVG(price) FILTER (WHERE housing_type = 'studio'), 2) AS average_price_by_studio,
-        ROUND(AVG(price) FILTER (WHERE housing_type = 'quitinete'), 2) AS average_price_by_quitinete,
-        ROUND(AVG(size_m2), 2) AS average_size_in_m2,
-        ROUND(AVG(bedroom_count), 2) AS average_bedroom_count,
-        ROUND(AVG(parking_count), 2) AS average_parking_count
-    FROM 
-        public.rent_data
-    GROUP BY
-        job_id,
-        DATE(datetime);
+            job_id,
+            total_count,
+            total_avg AS average_price,
+            total_avg_monthly_count,
+            total_avg_monthly,
+            total_avg_daily_count,
+            total_avg_daily,
+            apartamento_monthly_count,
+            apartamento_monthly_avg,
+            apartamento_daily_count,
+            apartamento_daily_avg,
+            casa_monthly_count,
+            casa_monthly_avg,
+            casa_daily_count,
+            casa_daily_avg,
+            chacara_monthly_count,
+            chacara_monthly_avg,
+            chacara_daily_count,
+            chacara_daily_avg,
+            cobertura_monthly_count,
+            cobertura_monthly_avg,
+            cobertura_daily_count,
+            cobertura_daily_avg,
+            condominio_monthl_count,
+            condominio_monthly_avg,
+            condominio_daily_count,
+            condominio_daily_avg,
+            flat_monthly_count,
+            flat_daily_count,
+            flat_daily_avg,
+            loft_monthly_count,
+            loft_monthly_avg,
+            loft_daily_count,
+            loft_daily_avg,
+            quitinete_monthly_count,
+            quitinete_monthly_avg,
+            quitinete_daily_count,
+            quitinete_daily_avg,
+            sobrado_monthly_count,
+            sobrado_monthly_avg,
+            sobrado_daily_count,
+            sobrado_daily_avg,
+            studio_monthly_count,
+            studio_monthly_avg,
+            studio_daily_count,
+            studio_daily_avg,
+            vila_monthly_count,
+            vila_monthly_avg,
+            vila_daily_count,
+            vila_daily_avg
+        FROM public.overview
+        GROUP BY job_id;
+
     
         `, []);
     
         const data_overview_array: DataOverview[] = [];
     
-        for (const data_row of residential_properties_data){
-          const data_overview = new DataOverview(
-            data_row.job_id,
-            data_row.date,
-            parseFloat(data_row.average_price),
-            parseFloat(data_row.average_price_by_rent_type_monthly),
-            parseFloat(data_row.average_price_by_rent_type_daily),
-            parseFloat(data_row.average_price_by_apartamento),
-            parseFloat(data_row.average_price_by_sobrado),
-            parseFloat(data_row.average_price_by_casa),
-            parseFloat(data_row.average_price_by_studio),
-            parseFloat(data_row.average_price_by_quitinete),
-            parseFloat(data_row.average_size_in_m2),
-            parseFloat(data_row.average_bedroom_count),
-            parseFloat(data_row.average_parking_count)
-          );
+        for (const data_row of residential_properties_data) {
+    const data_overview = new DataOverview(
+        data_row.job_id,
+        data_row.total_count,
+        data_row.average_price,
+        data_row.total_avg_monthly_count,
+        data_row.total_avg_monthly,
+        data_row.total_avg_daily_count,
+        data_row.total_avg_daily,
+        data_row.apartamento_monthly_count,
+        data_row.apartamento_monthly_avg,
+        data_row.apartamento_daily_count,
+        data_row.apartamento_daily_avg,
+        data_row.casa_monthly_count,
+        data_row.casa_monthly_avg,
+        data_row.casa_daily_count,
+        data_row.casa_daily_avg,
+        data_row.chacara_monthly_count,
+        data_row.chacara_monthly_avg,
+        data_row.chacara_daily_count,
+        data_row.chacara_daily_avg,
+        data_row.cobertura_monthly_count,
+        data_row.cobertura_monthly_avg,
+        data_row.cobertura_daily_count,
+        data_row.cobertura_daily_avg,
+        data_row.condominio_monthl_count,
+        data_row.condominio_monthly_avg,
+        data_row.condominio_daily_count,
+        data_row.condominio_daily_avg,
+        data_row.flat_monthly_count,
+        data_row.flat_daily_count,
+        data_row.flat_daily_avg,
+        data_row.loft_monthly_count,
+        data_row.loft_monthly_avg,
+        data_row.loft_daily_count,
+        data_row.loft_daily_avg,
+        data_row.quitinete_monthly_count,
+        data_row.quitinete_monthly_avg,
+        data_row.quitinete_daily_count,
+        data_row.quitinete_daily_avg,
+        data_row.sobrado_monthly_count,
+        data_row.sobrado_monthly_avg,
+        data_row.sobrado_daily_count,
+        data_row.sobrado_daily_avg,
+        data_row.studio_monthly_count,
+        data_row.studio_monthly_avg,
+        data_row.studio_daily_count,
+        data_row.studio_daily_avg,
+        data_row.vila_monthly_count,
+        data_row.vila_monthly_avg,
+        data_row.vila_daily_count,
+        data_row.vila_daily_avg
+    );
           data_overview_array.push(data_overview)
         }
         return data_overview_array;    
     }
     async findAllByJobId(job_id: number): Promise<DataOverview> {
-        const residential_properties_data = await this.connection.query(`
-            SELECT 
+        const query = `
+            SELECT
                 job_id,
-                DATE(datetime) AS date,
-                ROUND(AVG(price), 2) AS average_price,
-                ROUND(AVG(price) FILTER (WHERE rent_type = 'monthly'), 2) AS average_price_by_rent_type_monthly,
-                ROUND(AVG(price) FILTER (WHERE rent_type = 'daily'), 2) AS average_price_by_rent_type_daily,
-                ROUND(AVG(price) FILTER (WHERE housing_type = 'apartamento'), 2) AS average_price_by_apartamento,
-                ROUND(AVG(price) FILTER (WHERE housing_type = 'sobrado'), 2) AS average_price_by_sobrado,
-                ROUND(AVG(price) FILTER (WHERE housing_type = 'casa'), 2) AS average_price_by_casa,
-                ROUND(AVG(price) FILTER (WHERE housing_type = 'studio'), 2) AS average_price_by_studio,
-                ROUND(AVG(price) FILTER (WHERE housing_type = 'quitinete'), 2) AS average_price_by_quitinete,
-                ROUND(AVG(size_m2), 2) AS average_size_in_m2,
-                ROUND(AVG(bedroom_count), 2) AS average_bedroom_count,
-                ROUND(AVG(parking_count), 2) AS average_parking_count
-            FROM 
-                public.rent_data
+                total_count,
+                total_avg AS average_price,
+                total_avg_monthly_count,
+                total_avg_monthly,
+                total_avg_daily_count,
+                total_avg_daily,
+                apartamento_monthly_count,
+                apartamento_monthly_avg,
+                apartamento_daily_count,
+                apartamento_daily_avg,
+                casa_monthly_count,
+                casa_monthly_avg,
+                casa_daily_count,
+                casa_daily_avg,
+                chacara_monthly_count,
+                chacara_monthly_avg,
+                chacara_daily_count,
+                chacara_daily_avg,
+                cobertura_monthly_count,
+                cobertura_monthly_avg,
+                cobertura_daily_count,
+                cobertura_daily_avg,
+                condominio_monthl_count,
+                condominio_monthly_avg,
+                condominio_daily_count,
+                condominio_daily_avg,
+                flat_monthly_count,
+                flat_daily_count,
+                flat_daily_avg,
+                loft_monthly_count,
+                loft_monthly_avg,
+                loft_daily_count,
+                loft_daily_avg,
+                quitinete_monthly_count,
+                quitinete_monthly_avg,
+                quitinete_daily_count,
+                quitinete_daily_avg,
+                sobrado_monthly_count,
+                sobrado_monthly_avg,
+                sobrado_daily_count,
+                sobrado_daily_avg,
+                studio_monthly_count,
+                studio_monthly_avg,
+                studio_daily_count,
+                studio_daily_avg,
+                vila_monthly_count,
+                vila_monthly_avg,
+                vila_daily_count,
+                vila_daily_avg
+            FROM public.overview
             WHERE 
                 job_id = $1
             GROUP BY
-                job_id, 
-                DATE(datetime);
-        `, [job_id]);
-
-        const data_row = residential_properties_data[0]; 
-
-        const data_overview: DataOverview = new DataOverview(
-        data_row.job_id,
-        data_row.date,
-        parseFloat(data_row.average_price),
-        parseFloat(data_row.average_price_by_rent_type_monthly),
-        parseFloat(data_row.average_price_by_rent_type_daily),
-        parseFloat(data_row.average_price_by_apartamento),
-        parseFloat(data_row.average_price_by_sobrado),
-        parseFloat(data_row.average_price_by_casa),
-        parseFloat(data_row.average_price_by_studio),
-        parseFloat(data_row.average_price_by_quitinete),
-        parseFloat(data_row.average_size_in_m2),
-        parseFloat(data_row.average_bedroom_count),
-        parseFloat(data_row.average_parking_count)
-        );   
-
+                job_id
+        `;
+        
+        const result = await this.connection.query(query, [job_id]);
+       
+        const data_row = result[0];
+    
+        const data_overview = new DataOverview(
+            data_row.job_id,
+            data_row.total_count,
+            data_row.average_price,
+            data_row.total_avg_monthly_count,
+            data_row.total_avg_monthly,
+            data_row.total_avg_daily_count,
+            data_row.total_avg_daily,
+            data_row.apartamento_monthly_count,
+            data_row.apartamento_monthly_avg,
+            data_row.apartamento_daily_count,
+            data_row.apartamento_daily_avg,
+            data_row.casa_monthly_count,
+            data_row.casa_monthly_avg,
+            data_row.casa_daily_count,
+            data_row.casa_daily_avg,
+            data_row.chacara_monthly_count,
+            data_row.chacara_monthly_avg,
+            data_row.chacara_daily_count,
+            data_row.chacara_daily_avg,
+            data_row.cobertura_monthly_count,
+            data_row.cobertura_monthly_avg,
+            data_row.cobertura_daily_count,
+            data_row.cobertura_daily_avg,
+            data_row.condominio_monthl_count,
+            data_row.condominio_monthly_avg,
+            data_row.condominio_daily_count,
+            data_row.condominio_daily_avg,
+            data_row.flat_monthly_count,
+            data_row.flat_daily_count,
+            data_row.flat_daily_avg,
+            data_row.loft_monthly_count,
+            data_row.loft_monthly_avg,
+            data_row.loft_daily_count,
+            data_row.loft_daily_avg,
+            data_row.quitinete_monthly_count,
+            data_row.quitinete_monthly_avg,
+            data_row.quitinete_daily_count,
+            data_row.quitinete_daily_avg,
+            data_row.sobrado_monthly_count,
+            data_row.sobrado_monthly_avg,
+            data_row.sobrado_daily_count,
+            data_row.sobrado_daily_avg,
+            data_row.studio_monthly_count,
+            data_row.studio_monthly_avg,
+            data_row.studio_daily_count,
+            data_row.studio_daily_avg,
+            data_row.vila_monthly_count,
+            data_row.vila_monthly_avg,
+            data_row.vila_daily_count,
+            data_row.vila_daily_avg
+        );
+    
         return data_overview;
-    }    
+    }
+        
 }
